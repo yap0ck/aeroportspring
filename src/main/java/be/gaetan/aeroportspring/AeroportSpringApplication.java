45b8e16a738f4ebfaf5309a6f99d82186566c486
Beukens <beukensgaetan@gmail.com>
2023-12-13 09:12:54 +0100

    init
    creation de application.yml

---
+++ src/main/java/be/gaetan/aeroportspring/AeroportSpringApplication.java
@@ -0,0 +1,13 @@
+package be.gaetan.aeroportspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroportSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeroportSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/gaetan/aeroportspring/AeroportSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package be.gaetan.aeroportspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroportSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
