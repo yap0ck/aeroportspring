cfeec6f3930731ed2c78f905cde1ae0eda91799a
Beukens <beukensgaetan@gmail.com>
2023-12-14 11:12:15 +0100

    Improve query methods for verifier and repairman Ids
    The commit introduces improved query methods for verifier and repairman in 'InterventionRepository'. It adds custom queries with the use of @Query annotation to fetch interventions by specific verifier and repairman's Ids, which enhances data filtering and retrieval efficiency.

--- src/main/java/be/gaetan/aeroportspring/dal/repositories/InterventionRepository.java
+++ src/main/java/be/gaetan/aeroportspring/dal/repositories/InterventionRepository.java
@@ -3,6 +3,8 @@ package be.gaetan.aeroportspring.dal.repositories;
 import be.gaetan.aeroportspring.dal.models.Intervention;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +12,8 @@ import java.util.List;
 @Repository
 public interface InterventionRepository extends JpaRepository<Intervention, Long>, JpaSpecificationExecutor<Intervention> {
     List<Intervention> findAllByDeleted(boolean deleted);
-    List<Intervention> findAllByReparateur(long id);
-    List<Intervention>findAllByVerificateur(long id);
+    @Query("SELECT i FROM Intervention i JOIN i.reparateur r WHERE r.id = :reparateur_id")
+    List<Intervention> findAllByReparateur(@Param("reparateur_id") long id);
+    @Query("SELECT i FROM Intervention i JOIN i.verificateur v WHERE v.id = :verificateur_id")
+    List<Intervention>findAllByVerificateur(@Param("verificateur_id") long id);
 }
